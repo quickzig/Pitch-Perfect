6e87e3a6e325eec423d90be9fb893347de553c42
Quickzig <brian.quick@gmail.com>
2015-06-10 21:19:31 -0500

    Initial upload of Pitch Perfect
    Work in progress. This version speeds up and slows down a mp3 file

---
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by Brian Quick on 6/10/15.
+//  Copyright (c) 2015 Brian Quick. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundViewController: UIViewController {
+    var audioPlayer:AVAudioPlayer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
+        {
+            var fileUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: fileUrl, error: nil)
+            audioPlayer.enableRate = true
+        }
+        else
+        {
+        println("the FilePath is empty")
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowSound(sender: UIButton) {
+        playSound(0.5)
+    }
+    
+    @IBAction func playFastSound(sender: UIButton) {
+        playSound(2.0)
+    }
+    
+    func playSound(speed: Float )
+    {
+        audioPlayer.stop()
+        audioPlayer.rate = speed
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    
+   
+    @IBAction func stopSound(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+        recordButton.enabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Record the user's voice
+        recordingLabel.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+        
+        println("In recordAudio")
+        
+    }
 
+    @IBAction func recordingStop(sender: UIButton) {
+        recordingLabel.hidden = true
+    }
+    
+   
+    
 }
 
